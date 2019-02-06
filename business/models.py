7ca1d6313fa02676d0bbe16b18720ca6ed34a0f3
misun <sosgik96@naver.com>
2019-02-06 17:13:51 +0900

    commu

--- business/models.py
+++ business/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class BusinessPost(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
-
+    text = models.CharField(max_length=200)
     def __str__(self):
         return self.title
