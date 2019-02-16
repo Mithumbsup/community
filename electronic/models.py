a15f8d0ca2c87607f255b0d80fac9a8ae3d168fb
Mithumbsup <46515334+Mithumbsup@users.noreply.github.com>
2019-02-16 15:55:29 +0900

    Update models.py

--- electronic/models.py
+++ electronic/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class ElectronicPost(models.Model):
+class EngineerPost(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
+    approved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+    def approve(self):
+        self.approved = True
+        self.save()
