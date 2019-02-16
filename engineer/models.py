75eb1db18a8c7b2c832d73c942d4216d63eb8067
Mithumbsup <46515334+Mithumbsup@users.noreply.github.com>
2019-02-16 15:56:11 +0900

    Update models.py

--- engineer/models.py
+++ engineer/models.py
@@ -4,6 +4,11 @@ from django.db import models
 class EngineerPost(models.Model):
     title = models.CharField(max_length=20)
     content = models.TextField()
+    approved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
+
+    def approve(self):
+        self.approved = True
+        self.save()
